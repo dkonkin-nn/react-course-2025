44d7a3b7889538ad2389f4c9bdd05788190f0e1b
dkonkin-nn <deli.is.play@gmail.com>
2025-06-04 20:29:26 +0300

    Проданные товары не удаляю, а делаю 'Недоступными'

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -2,16 +2,16 @@ import "./Product.css"
 
 function Product(props) {
 
-    const { photoName: srcPhoto, name, description, price } = props.product;
+    const { photoName: srcPhoto, name, description, price, soldOut } = props.product;
     return (
-        <li className="product">
+        <li className={soldOut ? "product sold-out" : "product"} >
             <img src={srcPhoto} alt="" />
             <div>
                 <h3>{name}</h3>
                 <p>{description}</p>
                 <span>{price}</span>
             </div>
-        </li>
+        </li >
     )
 }
 
